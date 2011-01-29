0300808b8bb282a95896c37b85f4543bb72a2c77
Afshin Darian <afshin@albion.(none)>
2011-01-29 14:47:18 +0000

    updating solution 9 to node framework

--- 009.js
+++ 009.js
@@ -1,15 +1,13 @@
-(function(){
-    var time_start = Date.now();
-    var a, b, c;
+var euler = require('./common/euler');
+var main = function(){
+    var a, b, c, result = [];
     for (c = 1000; c > 2; c -= 1){
         for (b = c - 1; b > 1; b -= 1){
             for (a = b - 1; a > 0; a -= 1){
-                if (((a * a) + (b * b) == (c * c)) && ((a + b + c) == 1000)){
-                    java.lang.System.out.println([a, b, c].join(', '));
-                };
+                if (((a * a) + (b * b) == (c * c)) && ((a + b + c) == 1000)) result.push([a, b, c].join(', '));
             };
         };
     };
-    var time_end = Date.now();
-    java.lang.System.out.println('\ndone in ' + (time_end - time_start) + ' ms.');
-})();
+    return result.join('\n');
+};
+euler.run(main);
