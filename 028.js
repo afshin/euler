7441468048364c2eba11e405ea1192d09a47573a
Afshin Darian <afshin@albion.home>
2011-02-05 19:06:19 +0000

    updated euler.js to read files if they have the same name as current problem and pass in value, used in 018 and 067

--- 028.js
+++ 028.js
@@ -1,13 +1,10 @@
 require('./common/euler').run(function () {
     var a = [1], b = [1], c = [1], d = [1], lcv, len;
-    var sum = function (arr) {
-        return arr.reduce(function (acc, i) {return acc + (i || 0);}, 0);
-    };
     for (lcv = 3; lcv < 1002; lcv += 2) a.push(lcv * lcv);
     for (lcv = 1, len = a.length; lcv < len; lcv += 1){
         b[lcv] = a[lcv] - (4 * lcv);
         c[lcv] = b[lcv] - (2 * lcv);
         d[lcv] = c[lcv] + (4 * lcv);
     };
-    return 'sum is: ' + (sum([sum(a), sum(b), sum(c), sum(d)]) - 3);
+    return 'sum is: ' + ([a.sum(), b.sum(), c.sum(), d.sum()].sum() - 3);
 });
--- common/euler.js
+++ common/euler.js
@@ -1,32 +1,38 @@
-var print = require('sys').print;
+var print = require('sys').print, fs = require('fs'), path = require('path');
+
 // array tail-recursive binary search with customizable comparator
-Array.prototype.search = function(value, fn, low, high, mid){
+Array.prototype.search = function (value, fn, low, high, mid) {
     if (high < low) return ((low === 0) ? -0.1 : -1 * low);
-    var comparator = fn || function(x){return value - x;};
+    var comparator = fn || function (x) {return value - x;};
     if (arguments.length < 5) return this.search(value, fn, 0, this.length - 1, Math.floor((this.length -  1) / 2));
     var potential = comparator(this[mid]);
     if (potential > 0) return this.search(value, comparator, mid + 1, high, Math.floor((mid + 1 + high) / 2));
     if (potential < 0) return this.search(value, comparator, low, mid - 1, Math.floor((low + mid - 1) / 2))
     return mid;
 };
-Array.prototype.insert = function(value){ // assumes sorted array, returns new array
+
+Array.prototype.insert = function (value) { // assumes sorted array, returns new array
     var index = Math.abs(this.search(value));
     return this.slice(0, index).concat(value).concat(this.slice(index, this.length));
 };
 
-exports.debug = function(x){
+Array.prototype.sum = function () {
+    return this.reduce(function (acc, i) {return acc + (i || 0);}, 0);
+};
+
+exports.debug = function (x) {
     print('[debug] ' + x + '\n');
 };
 
-exports.divisors = function(num){
+exports.divisors = function (num) {
     var divisors = [1], limit = Math.sqrt(num) + 1, lcv;
     for (lcv = 2; lcv < limit; lcv += 1) if (0 === num % lcv) divisors.push(lcv, num / lcv);
     return divisors;
 };
 
-exports.is_prime = (function(){
+exports.is_prime = (function () {
     var memo = {0: false, 1: false, 2: true};
-    return function(num){
+    return function (num) {
         if (num in memo) return memo[num];
         if (0 === num % 2) return memo[num] = false;
         for (var lcv = 3; Math.floor(num / lcv) + 2 > lcv; lcv += 2)
@@ -35,14 +41,14 @@ exports.is_prime = (function(){
     };
 })();
 
-exports.permutations = (function(){
+exports.permutations = (function () {
     /* use with a sorted array if you want numbers in order */
-    var permutations = function(arr, depth){
-        var len = arr.length, depth = depth || len;
+    var permutations = function (arr, depth) {
+        var lcv, len = arr.length, depth = depth || len;
         if (len === 0) return [];
         if (len === 1) return [arr[0]];
-        for (var lcv = 0, result = []; lcv < depth; lcv += 1)
-            permutations(arr.slice(0, lcv).concat(arr.slice(lcv + 1, len))).map(function(p){
+        for (lcv = 0, result = []; lcv < depth; lcv += 1)
+            permutations(arr.slice(0, lcv).concat(arr.slice(lcv + 1, len))).map(function (p) {
                 result.push(+[arr[lcv], p].join(''));
             });
         return result;
@@ -50,9 +56,16 @@ exports.permutations = (function(){
     return permutations;
 })();
 
-exports.run = function(fn){
-    var time_start = Date.now();
-    var result = fn.apply(exports, null);
-    var time_end = Date.now();
-    print(result + '\nIt took ' + (time_end - time_start) + ' ms\n');
+exports.run = function (fn) {
+    var time_start = Date.now(), result, time_end, data_file, run;
+    data_file = process.argv[1].replace(/\.js$/gi, '.txt');
+    run = function (data) {
+        result = fn.apply(exports, data);
+        time_end = Date.now();
+        print(result + '\nIt took ' + (time_end - time_start) + ' ms\n');
+    };
+    path.exists(data_file, function (exists) {
+        if (!exists) return run(null);
+        fs.readFile(data_file, 'utf-8', function (err, data) {run([data]);});
+    });
 };
