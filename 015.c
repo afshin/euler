4c8e8e98445edc461cad0a5d91907b1c502ad958
Afshin Darian <afshin@albion.home>
2011-02-06 12:07:40 +0000

    removing 015.c, the JS solution runs in a more realistic time

--- 015.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int paths(long int x, long int y){
-    if (x == 1 || y == 1) return x + y;
-    return paths(x - 1, y) + paths(x, y - 1);
-};
-int main(){
-    printf("paths(20, 20): %d\n", paths(20, 20));
-    return 0;
-};
