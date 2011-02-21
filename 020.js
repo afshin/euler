9c09692574e70454ab7fe95f9f21333671221302
Afshin Darian <afshin@albion.home>
2011-02-21 20:04:49 +0000

    adding usage line to 020.js

--- 020.js
+++ 020.js
@@ -1,5 +1,5 @@
-// python 020.py generates the factorial number that is passed in via the -in argument
-// because python does long numbers and JS doesn't
+// python 020.py generates the factorial number that is passed in because python does long numbers and JS doesn't
+// usage: python 020.py | node 020.js -in
 require('./common/euler').run(function (input) {
     return input.split('').reduce(function (acc, val) {return +val + acc;}, 0);
 });
