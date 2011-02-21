edcaf2e240e86d33f3a01ad8f2ed8624b876c858
Afshin Darian <afshin@albion.home>
2011-02-21 22:56:02 +0000

    formatting

--- 018.js
+++ 018.js
@@ -1,15 +1,14 @@
-require('./common/euler').run(function (input_file) {
-    var data = input_file.split('\n').map(function (line) {
-            return line.split(' ').map(function (item) {return +item;});
-        }), largest_routes = {}, total_levels = data.length - 1,
-        largest_route = function (starting_point) {
-            var level = starting_point[0], index = starting_point[1], value = data[level][index],
-                route = [level, index].join('-'), left, right;
-            if (level === total_levels) return largest_routes[route] = [value];
+require('./common/euler').run(function (input) {
+    var data = input.split('\n').map(function (line) {return line.split(' ').map(function (item) {return +item;});}),
+        largest_routes = {}, last_level = data.length - 1,
+        largest_route = function (point) {
+            var level = point[0], index = point[1], value = data[level][index],
+                route = point.join('-'), left, right, next = level + 1;
+            if (level === last_level) return largest_routes[route] = [value];
             if (route in largest_routes) return largest_routes[route];
-            if (level === total_levels - 1)
-                return largest_routes[route] = (value + data[level + 1][index]) > (value + data[level + 1][index + 1]) ? 
-                    [value, data[level + 1][index]] : [value, data[level + 1][index + 1]];
+            if (level === last_level - 1)
+                return largest_routes[route] = (value + data[next][index]) > (value + data[next][index + 1]) ? 
+                    [value, data[next][index]] : [value, data[next][index + 1]];
             left = [value].concat(largest_route([level + 1, index]));
             right = [value].concat(largest_route([level + 1, index + 1]));
             return largest_routes[route] = left.sum() > right.sum() ? left : right;
