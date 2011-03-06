15696e06d28710ce4a2c87b6bd0dccf1901f3cb9
Afshin Darian <afshin@albion.home>
2011-03-06 10:34:33 +0000

    slight tweak on 038

--- 038.js
+++ 038.js
@@ -1,9 +1,9 @@
 require('./common/euler').run(function () {
     var euler = this, nums = euler.permutations([9, 8, 7, 6, 5, 4, 3, 2, 1], 1).sort(), lcv, len = nums.length;
     var is_match = (function () {
-        var max = [null, null, 9999, 333, 99], min = [null, null, 5000, 100, 10], memo = [];
+        var max = [null, null, 9999, 333, 99], min = [null, null, 5000, 100, 25], memo = [];
         return function (num) {
-            for (var i = 2, j, k, range, current; i < 6; i += 1){
+            for (var i = 2, j, k, range, current; i < 5; i += 1){
                 range = memo[i] || (memo[i] = euler.range(1, i));
                 if (!range.cache) range.cache = [];
                 for (j = min[i]; j < max[i]; j += 1){
