dbf23b124a0a9a1b31f67bbb2c23fde98a524941
Afshin Darian <afshin@albion.home>
2011-03-06 02:14:40 +0000

    some speed optimization on 038

--- 038.js
+++ 038.js
@@ -1,14 +1,12 @@
 require('./common/euler').run(function () {
     // find largest 9 digit pandigital whose written values equal {1, 2, ... n} * x where n >= 2 and x is an integer
-    var euler = this, nums = euler.permutations([9, 8, 7, 6, 5, 4, 3, 2, 1], 1), lcv, len = nums.length;
-    nums = nums.sort().reverse();
+    var euler = this, nums = euler.permutations([9, 8, 7, 6, 5, 4, 3, 2, 1], 1).sort(), lcv, len = nums.length;
     var is_match = (function () {
-        var max = [null, null, 9999, 999, 999, 99, 99, 9];
+        var max = [null, null, 9999, 500, 99, 99], min = [null, null, 4000, 100, 10, 10];
         return function (num) {
-            var i, j, range, current;
-            for (i = 2; i < 8; i += 1){
+            for (var i = 2, j, range, current; i < 7; i += 1){
                 range = euler.range(1, i);
-                for (j = 2; j < max[i]; j += 1){
+                for (j = min[i]; j < max[i]; j += 1){
                     current = range.map(function (v) {return v * j;}).join('');
                     if (current.length > 9) break; else if (num === current) return true;
                 };
@@ -16,5 +14,5 @@ require('./common/euler').run(function () {
             return false;
         };
     })();
-    for (lcv = 0; lcv < len; lcv += 1) if (is_match(nums[lcv])) return nums[lcv];
+    for (lcv = len - 1; lcv > 0; lcv -= 1) if (is_match(nums[lcv])) return nums[lcv];
 });
