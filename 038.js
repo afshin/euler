3950738d633fd18c5016558e7174e381edbfb8c9
Afshin Darian <afshin@albion.home>
2011-03-06 10:23:21 +0000

    better bounds + memoization = speed improvement

--- 038.js
+++ 038.js
@@ -1,13 +1,13 @@
 require('./common/euler').run(function () {
-    // find largest 9 digit pandigital whose written values equal {1, 2, ... n} * x where n >= 2 and x is an integer
     var euler = this, nums = euler.permutations([9, 8, 7, 6, 5, 4, 3, 2, 1], 1).sort(), lcv, len = nums.length;
     var is_match = (function () {
-        var max = [null, null, 9999, 500, 99, 99], min = [null, null, 4000, 100, 10, 10];
+        var max = [null, null, 9999, 333, 99], min = [null, null, 5000, 100, 10], memo = [];
         return function (num) {
-            for (var i = 2, j, range, current; i < 7; i += 1){
-                range = euler.range(1, i);
+            for (var i = 2, j, k, range, current; i < 6; i += 1){
+                range = memo[i] || (memo[i] = euler.range(1, i));
+                if (!range.cache) range.cache = [];
                 for (j = min[i]; j < max[i]; j += 1){
-                    current = range.map(function (v) {return v * j;}).join('');
+                    current = range.cache[j] || (range.cache[j] = range.map(function (v) {return v * j}).join(''));
                     if (current.length > 9) break; else if (num === current) return true;
                 };
             };
