2cab739b99a75dd535675bcf2d2630963638b511
Afshin Darian <ishtar@gmail.com>
2011-10-23 13:31:47 +0100

    should have been a while loop, duh

--- 040.js
+++ 040.js
@@ -1,6 +1,6 @@
 require('./common/euler').run(function () {
-    var fraction = ['.'], lcv, answer = 1, exp = 6, length = 1000000;
-    for (lcv = 1; true; lcv += 1) if (Array.prototype.push.apply(fraction, (lcv + '').split('')) >= length) break;
+    var fraction = ['.'], lcv = 1, answer = 1, exp = 6, length = 1000000;
+    while (true) if (Array.prototype.push.apply(fraction, ('' + lcv++).split('')) >= length) break;
     for (lcv = 0; lcv < exp; lcv += 1) answer *= +fraction[Math.pow(10, lcv)];
     return answer;
 });
