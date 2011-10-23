277310530308e72b096e54dfa7033290f013f54e
Afshin Darian <ishtar@gmail.com>
2011-10-23 13:34:12 +0100

    stop looking up Array.prototype.push each time

--- 040.js
+++ 040.js
@@ -1,6 +1,6 @@
 require('./common/euler').run(function () {
-    var fraction = ['.'], lcv = 1, answer = 1, exp = 6, length = 1000000;
-    while (true) if (Array.prototype.push.apply(fraction, ('' + lcv++).split('')) >= length) break;
+    var fraction = ['.'], lcv = 1, answer = 1, exp = 6, length = 1000000, push = Array.prototype.push;
+    while (true) if (push.apply(fraction, ('' + lcv++).split('')) >= length) break;
     for (lcv = 0; lcv < exp; lcv += 1) answer *= +fraction[Math.pow(10, lcv)];
     return answer;
 });
