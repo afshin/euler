0ddb2c709e8f39a9ca5367ec62ddaf82ad1f7cc0
Afshin Darian <ishtar@gmail.com>
2011-10-23 13:26:06 +0100

    move init values to the top

--- 040.js
+++ 040.js
@@ -1,6 +1,6 @@
 require('./common/euler').run(function () {
-    var fraction = ['.'], lcv, answer = 1;
-    for (lcv = 1; true; lcv += 1) if (Array.prototype.push.apply(fraction, (lcv + '').split('')) >= 1000000) break;
-    for (lcv = 0; lcv < 6; lcv += 1) answer *= +fraction[Math.pow(10, lcv)];
+    var fraction = ['.'], lcv, answer = 1, exp = 6, length = 1000000;
+    for (lcv = 1; true; lcv += 1) if (Array.prototype.push.apply(fraction, (lcv + '').split('')) >= length) break;
+    for (lcv = 0; lcv < exp; lcv += 1) answer *= +fraction[Math.pow(10, lcv)];
     return answer;
 });
