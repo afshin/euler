a93594503e2a4d4a02de91f11e1c5f58528e9bb2
Afshin Darian <ishtar@gmail.com>
2012-04-23 01:20:15 +0200

    didn't need euler lib at all for this one for this one

--- 044.js
+++ 044.js
@@ -1,5 +1,5 @@
 require('./common/euler').run(function () {
-    var euler = this, memo = {}, pentagonals = {}, j, k, bound = 2500,
+    var memo = {}, pentagonals = {}, j, k, bound = 2500,
         pentagonal = function (n) {pentagonals[memo[n] = (3 * n * n - n) / 2] = null;};
     for (j = bound; pentagonal(j), j > 0; j -= 1)
         for (k = 1; pentagonal(k), k < bound; k += 1)
