a76a59b3baf309023eaf2c8085368c1071b4e3ae
Afshin Darian <af@afbot.com>
2013-08-18 00:47:43 +0100

    small changes to 049

--- 049.js
+++ 049.js
@@ -5,26 +5,23 @@ require('./common/euler').run(function () {
         return Object.keys(this.reduce(function (acc, val) {return (acc[val + ''] = null), acc;}, {}));
     };
     var regular_subsets = function (arr) {
-        var available = arr.reduce(function (acc, val, idx) {return (acc[val] = idx), acc}, {}),
-            len = arr.length, diffs = arr.reduce(function (a, x, i) {
+        var available = arr.reduce(function (acc, val, idx) {return (acc[val] = idx), acc}, {});
+        return arr.reduce(function (a, x, i) {
                 return a.concat(arr.slice(i + 1).map(function (y) {return y - x;}))
-            }, []).unique().map(Number);
-        return diffs.map(function (diff) {
-            return arr.map(function (val) {
-                var subset = euler.range(val, arr[len - 1], diff)
-                    .filter(function (val) {return val in available;})
-                    .reduce(function (acc, val, idx) {
-                        return 0 === idx || diff === val - acc[acc.length - 1] ? acc.concat(val) : acc;
-                    }, []);
-                if (subset.length > 2) return subset;
-            }).filter(Boolean);
-        }).flatten().filter(Boolean);
+            }, [])
+            .unique().map(Number).map(function (diff) {
+                return arr.map(function (val) {
+                    return euler.range(val, arr[arr.length - 1], diff)
+                        .filter(function (val) {return val in available;})
+                        .reduce(function (acc, val, idx) {
+                            return 0 === idx || diff === val - acc[acc.length - 1] ? acc.concat(val) : acc;
+                        }, []);
+                }).filter(function (subset) {return subset.length > 2;});
+        }).flatten();
     };
     return euler.range(start, end)
-        .map(function (base) {return (base + '').split('').sort().join('');}).unique().map(function (key) {
-            return euler.permutations(key.split('')).map(Number)
-                .filter(function (x) {return x > start;})
+        .map(function (base) {return (base + '').split('').sort().join('');}).unique().map(function (base) {
+            return euler.permutations(base.split('')).map(Number).filter(function (val) {return val > start;})
                 .unique().map(Number).sort().filter(euler.is_prime);
-        }).filter(function (arr) {return arr.length > 2;})
-        .map(regular_subsets).flatten().filter(function (subset) {return subset.length > 2;}).join('\n');
+        }).filter(function (arr) {return arr.length > 2;}).map(regular_subsets).flatten().join('\n');
 });
