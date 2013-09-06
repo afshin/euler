31058a165e09fae8c57d9a3fc38f20122c9d7b4f
Afshin Darian <af@afbot.com>
2013-09-06 18:27:13 +0100

    minor changes

--- 049.js
+++ 049.js
@@ -1,23 +1,21 @@
 require('./common/euler').run(function () {
-    var euler = this, start = 1000, end = 9999;
+    var euler = this, has = 'hasOwnProperty', start = 1000, end = 9999;
     Array.prototype.flatten = function () {return this.reduce(function (acc, val) {return acc.concat(val);}, []);};
     Array.prototype.unique = function () {
-        return Object.keys(this.reduce(function (acc, val) {return (acc[val + ''] = null), acc;}, {}));
+        return Object.keys(this.reduce(function (acc, val) {return (acc[val] = null), acc;}, {}));
     };
     var regular_subsets = function (arr) {
         var available = arr.reduce(function (acc, val, idx) {return (acc[val] = idx), acc}, {});
-        return arr.reduce(function (a, x, i) {
-                return a.concat(arr.slice(i + 1).map(function (y) {return y - x;}))
-            }, [])
+        return arr.reduce(function (a, x, i) {return a.concat(arr.slice(i + 1).map(function (y) {return y - x;}))}, [])
             .unique().map(Number).map(function (diff) {
                 return arr.map(function (val) {
                     return euler.range(val, arr[arr.length - 1], diff)
-                        .filter(function (val) {return val in available;})
+                        .filter(function (val) {return available[has](val);})
                         .reduce(function (acc, val, idx) {
                             return 0 === idx || diff === val - acc[acc.length - 1] ? acc.concat(val) : acc;
                         }, []);
                 }).filter(function (subset) {return subset.length > 2;});
-        }).flatten();
+            }).flatten();
     };
     return euler.range(start, end)
         .map(function (base) {return (base + '').split('').sort().join('');}).unique().map(function (base) {
